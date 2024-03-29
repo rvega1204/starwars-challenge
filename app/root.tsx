74bf2503b9409d6fe53b5c0d07e4b625fc67e68d
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-03-29 15:26:48 -0600

    Formatter configs and a test on it

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,10 @@
 import {
-	Link,
-	Links,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from "@remix-run/react"
 import type { LinksFunction } from "@remix-run/node"
 import stylesheet from "~/tailwind.css?url"
@@ -15,93 +15,58 @@ import { MenuItem } from "~/components/custom/MenuItem"
 
 import { GetLeagues } from "~/services"
 export const loader = async () => {
-	const leagues = await GetLeagues()
+  const leagues = await GetLeagues()
 
-	return json({
-		leagues,
-	})
+  return json({
+    leagues,
+  })
 }
 export const links: LinksFunction = () => [
-	{ rel: "stylesheet", href: stylesheet },
+  { rel: "stylesheet", href: stylesheet },
 ]
 export function Layout({ children }: { children: React.ReactNode }) {
-	const { leagues } = useLoaderData<typeof loader>()
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body className="flex">
-				<Menu>
-					{leagues.map((league) => (
-						<MenuItem
-							title={league.name}
-							value={league.id.toString()}
-							key={league.id}
-						>
-							{league.seasons.map((season) => (
-								<>
-									<Link
-										key={season.year}
-										to={`/teams/${season.year}/${league.id}`}
-									>
-										{season.year}
+  const { leagues } = useLoaderData<typeof loader>()
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="flex">
+        <Menu>
+          {leagues.map((league) => (
+            <MenuItem
+              title={league.name}
+              value={league.id.toString()}
+              key={league.id}
+            >
+              {league.seasons.map((season) => (
+                <>
+                  <Link
+                    key={season.year}
+                    to={`/teams/${season.year}/${league.id}`}
+                  >
+                    {season.year}
 
-										<br />
-									</Link>
-								</>
-							))}
-						</MenuItem>
-					))}
-				</Menu>
-				<div id="detail" className="max-w-[calc(100vw - 250px)]">
-					{children}
-				</div>
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	)
+                    <br />
+                  </Link>
+                </>
+              ))}
+            </MenuItem>
+          ))}
+        </Menu>
+        <div id="detail" className="max-w-[calc(100vw - 250px)]">
+          {children}
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
 }
 
 export default function App() {
-	return <Outlet />
+  return <Outlet />
 }
-// //
-// import {
-//   Links,
-//   Meta,
-//   Outlet,
-//   Scripts,
-//   ScrollRestoration,
-// } from "@remix-run/react";
-// import type { LinksFunction } from "@remix-run/node";
-// import stylesheet from "~/tailwind.css?url";
-//
-// export const links: LinksFunction = () => [
-//   { rel: "stylesheet", href: stylesheet },
-// ];
-// export function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <Meta />
-//         <Links />
-//       </head>
-//       <body className="">
-//         {children}
-//         <ScrollRestoration />
-//         <Scripts />
-//       </body>
-//     </html>
-//   );
-// }
-//
-// export default function App() {
-//   return <Outlet />;
-// }
--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  tabWidth: 2,
-  semi: false,
-  singleQuote: false,
-  useTabs: true,
+	plugins: ["prettier-plugin-tailwindcss"],
+	tabWidth: 2,
+	semi: false,
+	singleQuote: false,
+	useTabs: false,
 }
 
 export default config
