038f69ff3fc9e7402936bd96e9f5274e4ee215e9
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-01 13:28:54 -0600

    Adding the root page with the loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,18 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getRootRequest } from "~/services";
+
+export const loader = async () => {
+  const rootData = await getRootRequest();
+  return json({ rootData });
+};
+
 export default function Index() {
+  const { rootData } = useLoaderData<typeof loader>();
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-5xl font-bold">This is the main Page</h1>
-      <p className="text-xl font-medium">
-        Select a Season + a League from the Sidebar Menu
-      </p>
+      <p className="text-xl font-medium">{JSON.stringify(rootData)}</p>
     </div>
   );
 }
