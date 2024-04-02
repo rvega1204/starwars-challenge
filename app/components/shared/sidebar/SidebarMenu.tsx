2520eaeee759ab00fcda0d9847bb3e0944acf269
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-02 09:49:18 -0600

    Adding the data to the navbar

--- app/components/shared/sidebar/SidebarMenu.tsx
+++ app/components/shared/sidebar/SidebarMenu.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { SidebarHeader } from "./SidebarHeader";
+import {
+  Command,
+  CommandGroup,
+  CommandItem,
+  CommandList,
+} from "~/components/ui/command";
+import { menuData } from "~/seed";
+import { redirect } from "@remix-run/node";
 
 export const SidebarMenu = () => {
   return (
     <div className="min-h-screen w-[300px] min-w-[300px] border-r p-4">
       <SidebarHeader />
-      SidebarMenu
+      <div className="grow">
+        <Command>
+          <CommandList>
+            <CommandGroup heading="Applications">
+              {menuData.map((data) => (
+                <CommandItem
+                  key={data.url}
+                  className="flex gap-2"
+                  onClick={() => redirect(data.url)}
+                >
+                  <span>{data.title}</span>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </div>
     </div>
   );
 };
