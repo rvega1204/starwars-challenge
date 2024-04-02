ea075015bbb384c36c8a3c1f08b34af0b4380896
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-02 09:35:23 -0600

    Adding initial sidebar menu

---
+++ app/components/shared/sidebar/SidebarHeader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const SidebarHeader = () => {
+  return (
+    <div className="flex items-center justify-between gap-2 rounded border p-4">
+      <div className="avatar h-8 w-8 rounded-full bg-emerald-500"></div>
+      <div className="">
+        <p className="text-[16px] font-bold">Star Wars Challenge</p>
+        <p>Interviews</p>
+      </div>
+    </div>
+  );
+};
---
+++ app/components/shared/sidebar/SidebarMenu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SidebarHeader } from "./SidebarHeader";
+
+export const SidebarMenu = () => {
+  return (
+    <div className="min-h-screen w-[300px] min-w-[300px] border-r p-4">
+      <SidebarHeader />
+      SidebarMenu
+    </div>
+  );
+};
