b65558cbd78e107c65c603b138760597c495638b
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-03 11:41:24 -0600

    challenge item

--- app/interfaces/challenge.ts
+++ app/interfaces/challenge.ts
@@ -1,5 +1,10 @@
 export interface Challenge {
   title: string;
   description: string;
-  items: string[];
+  items: ChallengeItem[];
+}
+
+export interface ChallengeItem {
+  description: string;
+  documentationUrl?: string;
 }
