046d67fa53fb1bd2b9293f959a6e4956fdca3215
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-04 15:49:36 -0600

    Adding darth vader ship to the avatar

--- app/components/shared/sidebar/sidebar-header.tsx
+++ app/components/shared/sidebar/sidebar-header.tsx
@@ -1,7 +1,9 @@
 export const SidebarHeader = () => {
   return (
-    <div className="flex items-center justify-between gap-2 rounded border p-4">
-      <div className="avatar h-8 w-8 rounded-full bg-emerald-500"></div>
+    <div className="flex items-center gap-2 rounded border p-4">
+      <div className="avatar flex h-8 w-8 items-center justify-center rounded-full">
+        <img src="/vader.png" alt="avatar" className="w-full" />
+      </div>
       <div className="">
         <p className="text-[16px] font-bold">Star Wars Challenge</p>
         <p>Interviews</p>
