5cfea4de85da1da452c96237e8299362155948d5
Bleer30 <stif2001@gmail.com>
2024-04-04 13:49:38 -0400

    change the links of the film challenge

--- app/data/films-challenge-data.ts
+++ app/data/films-challenge-data.ts
@@ -14,12 +14,12 @@ const filmsChallengeItemData: ChallengeItem[] = [
   {
     description:
       "Create a service that calls the endpoint for using the Films API with the episode id as parameter to get the film information like image, title, director, producer, release date. The information will be added inside the tabs content. Here you can design the interface and add tailwind class with your own styles.",
-    documentationUrl: "http https://swapi.dev/api/films/1",
+    documentationUrl: "https://swapi.dev/api/films/1",
   },
   {
     description:
       "Create a service that calls the endpoint for using the Films API with the episode id as parameter to get the characters list; loading each one in a different component (you can choose between Accordion, Combobox, Select or html list). When the user select each one, the information related at selected will be showed in a dialog. You can design the component selected and the dialog with your own styles. ",
-    documentationUrl: "http https://swapi.dev/api/films/1",
+    documentationUrl: "https://swapi.dev/api/films/1",
   },
 ];
 
