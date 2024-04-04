4a4e383cfe367b71b09cd3558616caba525e1936
Bleer30 <stif2001@gmail.com>
2024-04-04 13:16:08 -0400

    display information in the vehicles component

--- app/routes/vehicles.tsx
+++ app/routes/vehicles.tsx
@@ -1,7 +1,32 @@
+import { Link } from "@remix-run/react";
+import { vehiclesChallengeData } from "~/data";
+
 export default function Vehicles() {
   return (
     <div>
-      <h1 className="text-5xl font-bold">Vehicles</h1>
+      <h1 className="mb-5 text-2xl font-bold">{vehiclesChallengeData.title}</h1>
+      <hr className="mb-10" />
+      <span className="font-light text-gray-700">
+        {vehiclesChallengeData.description}
+      </span>
+
+      <div className="mt-10 px-10 font-light text-gray-700">
+        <ul className="list-disc">
+          {vehiclesChallengeData.items.map((item, index) => (
+            <li key={index}>
+              {item.description}{" "}
+              {item.documentationUrl && (
+                <Link
+                  to={item.documentationUrl}
+                  className="text-sky-700 hover:underline"
+                >
+                  Review documentation.
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
