a2de8dad7cbd7a165f88379eaa5fd3c16d4355a0
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-05 11:42:40 -0600

    Star wars challenge

--- app/components/characters/character-card.tsx
+++ app/components/characters/character-card.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { FC } from 'react';
+import { Character } from '~/interfaces';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
 
-export const CharacterCard = () => {
-  return <div>CharacterCard</div>;
+interface Props {
+  character: Character;
+}
+export const CharacterCard: FC<Props> = ({ character }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{character.name}</CardTitle>
+        <CardDescription>{character.homeworld}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Gender: {character.gender}</p>
+      </CardContent>
+    </Card>
+  );
 };
