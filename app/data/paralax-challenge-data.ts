ac8e1b9264b74ddfa9edca4bb50077c96f2779f4
Rdani2005 <daniel.sequeira@betanysports.eu>
2024-04-05 15:30:34 -0600

    Adding parallax effect data challenge

---
+++ app/data/paralax-challenge-data.ts
@@ -0,0 +1,26 @@
+import { Challenge, ChallengeItem } from "~/interfaces/challenge";
+
+const parallaxChallengeData: ChallengeItem[] = [
+  {
+    description:
+      "Create a background that contains stars",
+  },
+  {
+    description:
+      "Get images from Star Wars Starships.",
+  },
+  {
+    description:
+      "Using Tailwind CSS and CSS styles, scroll down the background stars, but not the starships.",
+  },
+  {
+    description: "When we are getting to the final page, the starships should land.",
+  },
+];
+
+export const parallaxChallengeData: Challenge = {
+  title: "Parallax Challenge",
+  description:
+    "In this challenge, participants will employ Taiwind CSS and CSS Styles to make a parallax effect. The main goal is to see the background moving while the startships are on their way to the final destination.",
+  items: parallaxChallengeData,
+};
